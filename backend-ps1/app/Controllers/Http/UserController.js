c5a5c2a7d2c25f533b0ea2469194ad8b714801b6
Gabriel Doyle Balk <gbalk@inf.ufsm.br>
2019-04-02 19:10:45 -0300

    instalação e configuração do eslint'

--- backend-ps1/app/Controllers/Http/UserController.js
+++ backend-ps1/app/Controllers/Http/UserController.js
@@ -1,14 +1,11 @@
-"use strict";
-
-const User = use("App/Models/User");
+const User = use('App/Models/User')
 
 class UserController {
-  async store({ request }) {
-    const data = request.only(["username", "email", "password", "birth"]);
-
-    const user = await User.create(data);
-    return user;
+  async store ({ request }) {
+    const data = request.only(['username', 'email', 'password', 'birth'])
+    const user = await User.create(data)
+    return user
   }
 }
 
-module.exports = UserController;
+module.exports = UserController
