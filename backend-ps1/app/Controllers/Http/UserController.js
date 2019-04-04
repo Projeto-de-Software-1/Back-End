2715dd80c5d6facfa0648119c44dc8148b19c0b2
Gabriel Doyle Balk <gbalk@inf.ufsm.br>
2019-04-04 15:21:18 -0300

    adicao de endereco junto com o usuario

--- backend-ps1/app/Controllers/Http/UserController.js
+++ backend-ps1/app/Controllers/Http/UserController.js
@@ -3,7 +3,9 @@ const User = use('App/Models/User')
 class UserController {
   async store ({ request }) {
     const data = request.only(['username', 'email', 'password', 'birth'])
+    const address = request.input('address')
     const user = await User.create(data)
+    await user.addresses().create(address)
     return user
   }
 }
